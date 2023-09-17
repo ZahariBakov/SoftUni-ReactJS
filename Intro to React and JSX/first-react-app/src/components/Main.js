842600f66f9e4ed9031564c4fde8788faadb7815
ZahariBakov <zaharibakov@gmail.com>
2023-09-17 20:03:38 +0300

    Update "first-react-app".

---
+++ Intro to React and JSX/first-react-app/src/components/Main.js
@@ -0,0 +1,11 @@
+function Main() {
+    return (
+        <main>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. At libero explicabo omnis facere asperiores mollitia sit vel iusto dolores deleniti.
+            </p>
+        </main>
+    );
+}
+
+export default Main;
