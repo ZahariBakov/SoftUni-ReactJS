e94818f93252ba48069458673fe8b7a9ff746191
ZahariBakov <zaharibakov@gmail.com>
2023-09-18 18:23:44 +0300

    Update React app "react-components-basics".

--- React Components - Basic Idea/react-components-basics/src/components/TodoList.js
+++ React Components - Basic Idea/react-components-basics/src/components/TodoList.js
@@ -16,26 +16,39 @@
 
 // export default TodoList;
 
+import {useState} from 'react';
 import TodoListItem from "./TodoListItem";
 
 function TodoList() {
-    let firstTask = 'Clean your room modified';
-    let firstColor = 'blue';
-    let person = {
-        name: 'Pesho',
-        age: 32,
-    };
+    // let todoState = useState(['Clean your room', 'Go shopping', 'Learn React', 'Learn function components']);
+
+    // let [todos, setTodos] = todoState;
+
+    // let firstTask = 'Clean your room modified';
+    // let firstColor = 'blue';
+    // let person = {
+    //     name: 'Pesho',
+    //     age: 32,
+    // };
+
+    let [todo, setTodo] = useState('initial Todo');
 
     return (
         <>
             <h2>Tasks</h2>
 
             <ul>
-                <TodoListItem color={ 'light' + firstColor }>{ firstTask }</TodoListItem>
+                <TodoListItem>{ todo }</TodoListItem>
+
+                {/* {todos.map(todo => <TodoListItem>{todo}</TodoListItem>)} */}
+
+                {/* <TodoListItem color={ 'light' + firstColor }>{ firstTask }</TodoListItem>
                 <TodoListItem color="red">Go shopping</TodoListItem>
                 <TodoListItem color="green" person={ person }>Learn react</TodoListItem>
-                <TodoListItem color="blue">Learn function components</TodoListItem>
+                <TodoListItem color="blue">Learn function components</TodoListItem> */}
             </ul>
+
+            <button onClick={() => console.log('clicked')}>Modify</button>
         </>
     );
 }
--- React Components - Basic Idea/react-components-basics/src/components/TodoListItem.js
+++ React Components - Basic Idea/react-components-basics/src/components/TodoListItem.js
@@ -1,6 +1,5 @@
 const TodoListItem = (props) => {
-    console.log(props);
-    return <li style={{ color: props.color }}>{props.children} - { props.person?.name }</li>
+    return <li>{props.children}</li>
 }
 
 export default TodoListItem;
