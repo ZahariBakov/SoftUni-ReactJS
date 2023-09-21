d7068f4705001c57d938fe1f8a5610b966ea8cd3
ZahariBakov <zaharibakov@gmail.com>
2023-09-21 09:54:47 +0300

    Update react app "react-todo-app".

---
+++ React Components - Deep Dive/react-todo-app/src/components/TodoItem.js
@@ -0,0 +1,5 @@
+export default function TodoItem({
+    text
+}) {
+    return <li>{text}</li>
+}
--- React Components - Deep Dive/react-todo-app/src/components/TodoList.js
+++ React Components - Deep Dive/react-todo-app/src/components/TodoList.js
@@ -1,16 +1,37 @@
-import uniqid from 'uniqid';
+import { useState, useEffect } from 'react';
+import TodoItem from "./TodoItem";
 
 function TodoList() {
-    const todos = [
-        {id: 1, text: "Clean my room"},
-        {id: 2, text: "Wash the dishes"},
-        {id: 3, text: "Go to the gym"},
-    ];
+    const [todos, setTodos] = useState([
+        { id: 1, text: "Clean my room" },
+        { id: 2, text: "Wash the dishes" },
+        { id: 3, text: "Go to the gym" },
+    ]);
+
+    useEffect(() => {
+        console.log('Mounted');
+    }, []);
+
+    const onTodoInputBlur = (e) => {
+        let todo = {
+            id: todos.length + 1,
+            text: e.target.value
+        };
+
+        setTodos([
+            ...todos,
+            todo
+        ])
+    };
 
     return (
-        <ul>
-            {todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
-        </ul>
+        <>
+            <label htmlFor="todo-name">Add Todo</label>
+            <input type="text" id='todo-name' onBlur={onTodoInputBlur} name='todo' />
+            <ul>
+                {todos.map(todo => <TodoItem key={todo.id} text={todo.text} />)}
+            </ul>
+        </>
     );
 }
 
