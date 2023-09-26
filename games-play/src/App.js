79e5df7b0f82abce6f2bc6187d3230b8729fc325
ZahariBakov <zaharibakov@gmail.com>
2023-09-26 09:54:20 +0300

    Add routes and navigation handler.

--- games-play/src/App.js
+++ games-play/src/App.js
@@ -1,7 +1,19 @@
 import Header from "./components/Header";
 import WelcomeWorld from "./components/WelcomeWorld";
+import GameCatalog from "./components/GameCatalog";
+import CreateGame from "./components/CreateGame";
+import Login from "./components/Login";
+import Register from './components/Register';
 
 function App() {
+    const routes = {
+        '/home': WelcomeWorld,
+        '/games': GameCatalog,
+        '/create-game': CreateGame,
+        '/login': Login,
+        '/register': Register,
+    };
+
     return (
         <div id="box">
             <Header />
