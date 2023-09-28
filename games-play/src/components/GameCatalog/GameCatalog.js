5cd29fa1b0c70522ef8bae293f14fd070cd8e16c
ZahariBakov <zaharibakov@gmail.com>
2023-09-28 16:24:22 +0300

    Show no game yes message.

--- games-play/src/components/GameCatalog/GameCatalog.js
+++ games-play/src/components/GameCatalog/GameCatalog.js
@@ -5,20 +5,23 @@ const GameCatalog = () => {
     const [games, setGames] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
-            .then(res => res.json())
-            .then(result => {
-                setGames(result)
-            });
+        setTimeout(() => {
+            fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
+                .then(res => res.json())
+                .then(result => {
+                    setGames(result);
+                });
+        }, 1000);
     }, []);
 
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
 
-            { games.map(x => <Gamecard game={x} />) }
-
-            <h3 className="no-articles">No games yet</h3>
+            {games.length > 0
+                ? games.map(x => <Gamecard key={x._id} game={x} />)
+                : <h3 className="no-articles">No games yet</h3>
+            }
         </section>
     );
 }
