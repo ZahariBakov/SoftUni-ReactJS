62c2ace01a4f264c7ceee7ca224aee0816cb9973
ZahariBakov <zaharibakov@gmail.com>
2023-09-28 09:47:07 +0300

    Add simple page router.

--- games-play/src/App.js
+++ games-play/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Header from "./components/Header";
 import WelcomeWorld from "./components/WelcomeWorld";
 import GameCatalog from "./components/GameCatalog";
@@ -6,25 +8,32 @@ import Login from "./components/Login";
 import Register from './components/Register';
 
 function App() {
+    const [page, setPage] = useState('/home');
+
     const routes = {
-        '/home': WelcomeWorld,
-        '/games': GameCatalog,
-        '/create-game': CreateGame,
-        '/login': Login,
-        '/register': Register,
+        '/home': <WelcomeWorld />,
+        '/games': <GameCatalog />,
+        '/create-game': <CreateGame />,
+        '/login': <Login />,
+        '/register': <Register />,
     };
+
+    const navigationChangeHandler = (path) => {
+        setPage(path);
+    }
 
     return (
         <div id="box">
-            <Header />
+            <Header
+                navigationChangeHandler={navigationChangeHandler}
+            />
 
             <main id="main-content">
-                <WelcomeWorld />
+                { routes[page] || <h2>No Page Found!</h2> }
             </main>
+
         </div>
     );
 }
 
 export default App;
-
-
