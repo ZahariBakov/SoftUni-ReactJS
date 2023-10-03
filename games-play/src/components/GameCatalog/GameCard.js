569708c40eba42c68d21ad1b360f08d86089402a
ZahariBakov <zaharibakov@gmail.com>
2023-10-03 09:53:40 +0300

    Update.

--- games-play/src/components/GameCatalog/GameCard.js
+++ games-play/src/components/GameCatalog/GameCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 // const ratingCalculator = React.lazy(() => import('../../utils/ratingCalculator'));
 
 const GameCard = ({
--- games-play/src/components/GameCatalog/GameCatalog.js
+++ games-play/src/components/GameCatalog/GameCatalog.js
@@ -1,8 +1,8 @@
+
 import { useEffect, useState, lazy, Suspense } from 'react';
 
 import * as gameService from '../../services/gameService';
 // import GameCard from './GameCard';
-
 const GameCard = lazy(() => import('./GameCard'));
 
 const GameCatalog = ({
@@ -22,7 +22,6 @@ const GameCatalog = ({
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-
             <Suspense fallback={<p>Loading...</p>}>
                 {games.length > 0
                     ? games.map(x => <GameCard key={x._id} game={x} navigationChangeHandler={navigationChangeHandler} />)
